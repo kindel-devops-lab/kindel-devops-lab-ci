b8af91f5a6a13e9c80a345cfbb332a74f9856deb
Dimitri Kindelberger <dimitri.kindelberger@telecom-sudparis.eu>
2026-09-15 21:56:54 +0200

    missed a slash

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def health_check():
 def message():
     return "<h1>Description Page</h1><p>The purpose of this website is to help me practice developing and optimizing an entire project using a DevSecOps approach.</p> <p>Specifically: Develop a web microservice, containerize it properly, automate testing, build an automated CI/CD pipeline</p>"
 
-@app.route('frog', methods=['GET'])
+@app.route('/frog', methods=['GET'])
 def show_frog():
     return '''<img src="/static/frog.jpg" alt="Frog" width="400">'''
 
